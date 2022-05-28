6d919d4860d342ce49bc7b3b28860d0abbc539ec
vh-machado <vh.machado.silva@gmail.com>
2022-05-28 17:14:41 -0300

    UI atualizada

--- src/App.js
+++ src/App.js
@@ -1,317 +1,197 @@
 import React, { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import "./App.css";
+import PulseChart from "./components/PulseChart";
+import { SettingsIcon } from '@chakra-ui/icons'
+
+import {
+  Flex,
+  Text,
+  Center,
+  Input,
+  Button,
+  Box,
+  WrapItem,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
+
 import { converteNrzi, convertePseudo, converte4B3T } from "./Algoritmos";
 
-var seqEntrada = ("0100001011101001");
+var seqEntrada = "0100001011101001";
 
 var seqNrzi = converteNrzi(seqEntrada);
-seqNrzi.push(seqNrzi[seqNrzi.length - 1])
+seqNrzi.push(seqNrzi[seqNrzi.length - 1]);
 var seqPseudo = convertePseudo(seqEntrada);
-seqPseudo.push(seqPseudo[seqPseudo.length - 1])
+seqPseudo.push(seqPseudo[seqPseudo.length - 1]);
 var seq4B3T = converte4B3T(seqEntrada);
-seq4B3T.push(seq4B3T[seq4B3T.length - 1])
+seq4B3T.push(seq4B3T[seq4B3T.length - 1]);
 
 seqEntrada += " ";
 
 const App = () => {
-  const [categoryNzri, setCategoryNzri] = useState([]);
-  const [dataNzri, setDataNzri] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const [categoryNrzi, setCategoryNrzi] = useState([]);
+  const [dataNrzi, setDataNrzi] = useState([]);
   const [categoryPseudo, setCategoryPseudo] = useState([]);
   const [dataPseudo, setDataPseudo] = useState([]);
   const [category4B3T, setCategory4B3T] = useState([]);
   const [data4B3T, setData4B3T] = useState([]);
 
   useEffect(() => {
-    const estadoNzri = [];
-    const numBitsNzri = [];
+    const estadoNrzi = [];
+    const numBitsNrzi = [];
     const estadoPseudo = [];
     const numBitsPseudo = [];
     const estado4B3T = [];
     const numBits4B3T = [];
 
     for (var i = 0; i < seqNrzi.length; i++) {
-      estadoNzri.push(seqNrzi[i]);
-      numBitsNzri.push(seqEntrada[i]);
+      estadoNrzi.push(seqNrzi[i]);
+      numBitsNrzi.push(seqEntrada[i]);
     }
 
     for (var j = 0; j < seqPseudo.length; j++) {
       estadoPseudo.push(seqPseudo[j]);
       numBitsPseudo.push(seqEntrada[j]);
     }
-    
+
     for (var h = 0; h < seq4B3T.length; h++) {
       estado4B3T.push(seq4B3T[h]);
-      if (h == (seq4B3T.length - 1)) {
-        numBits4B3T.push(" ")
+      if (h == seq4B3T.length - 1) {
+        numBits4B3T.push(" ");
       } else if (seq4B3T[h] == -1) {
-        numBits4B3T.push("-")
+        numBits4B3T.push("-");
       } else if (seq4B3T[h] == 1) {
-        numBits4B3T.push("+")
+        numBits4B3T.push("+");
       } else {
         numBits4B3T.push(seq4B3T[h]);
-      }            
+      }
     }
 
-    setCategoryNzri(numBitsNzri);
-    setDataNzri(estadoNzri);
+    setCategoryNrzi(numBitsNrzi);
+    setDataNrzi(estadoNrzi);
     setCategoryPseudo(numBitsPseudo);
     setDataPseudo(estadoPseudo);
     setCategory4B3T(numBits4B3T);
     setData4B3T(estado4B3T);
   });
   return (
-    <>
-      <div>
-        <Chart
-          options={{
-            colors: ["#00bfff"],
-            chart: {
-              id: "Nrzi-example",
-              toolbar: {
-                show: false,
-              },
-              fontFamily: "Rubik",
-            },
-            markers: {
-              size: 5,
-              colors: "mediumpurple",
-            },
-            xaxis: {
-              type: "category",
-              categories: categoryNzri,
-              labels: {
-                offsetX: 10,
-                offsetY: -5,
-              },
-              position: "top",
-            },
-            yaxis: {
-              tickAmount: 1,
-              labels: {
-                style: {
-                  colors: ["red", "green"],
-                  fontSize: "16px",
-                  fontWeight: 500,
-                  cssClass: "apexcharts-yaxis-label",
-                },
-                formatter: function (value) {
-                  if (value === 1) {
-                    value = "+";
-                    return value;
-                  } else if (value === -1) {
-                    value = "-";
-                    return value;
-                  } else {
-                    value = "0";
-                    return value;
-                  }
-                },
-              },
-            },
-            grid: {
-              xaxis: {
-                lines: {
-                    show: true
-                }
-              },
-              yaxis: {
-                lines: {
-                    show: false
-                }
-              }, 
-            },
-            stroke: {
-              curve: "stepline",
-            },
-            title: {
-              text: "NRZI",
-              align: "center",
-              style: {
-                fontSize:  "18px",
-                fontWeight:  600,
-                color:  "black",
-              },
-            },
-          }}
-          series={[
-            {
-              name: "series-1",
-              data: dataNzri,
-            },
-          ]}
-          type="line"
-          width={1000}
-          height={200}
-        />
+    <Flex flex="1" bg="#FBD561" h="100vh" direction="row" justify="center" pt="3" pb="3" pl="10" pr="10">
+      <Center bg="#FFFFFF" direction="row" borderRadius="18" shadow="xl">
+        <Center
+          flexDiretction="column"
+          h="100%"
+          w="280px"
+          p="3"
+        >
+          <Center flex="1" flexDirection="column" alignSelf="flex-start">
+            <Center flex="1" bg="#FFF7EE" flexDirection="column" m="3" borderRadius="20" p="3" pb="5">
+              
+              <SettingsIcon/>
 
-        <Chart
-          options={{
-            colors: ["#ff69b4"],
-            chart: {
-              id: "pseudo-example",
-              toolbar: {
-                show: false,
-              },
-              fontFamily: "Rubik",
-            },
-            markers: {
-              size: 5,
-              colors: "crimson",
-            },
-            xaxis: {
-              type: "category",
-              categories: categoryPseudo,
-              labels: {
-                offsetX: 10,
-                offsetY: -5,
-              },
-              position: "top",
-            },
-            yaxis: {
-              tickAmount: 2,
-              labels: {
-                style: {
-                  colors: ["red", "black", "green"],
-                  fontSize: "16px",
-                  fontFamily: "Rubik",
-                  fontWeight: 500,
-                  cssClass: "apexcharts-yaxis-label",
-                },
-                formatter: function (value) {
-                  if (value === 1) {
-                    value = "+";
-                    return value;
-                  } else if (value === -1) {
-                    value = "-";
-                    return value;
-                  } else {
-                    value = "0";
-                    return value;
-                  }
-                },
-              },
-            },
-            grid: {
-              xaxis: {
-                lines: {
-                    show: true
-                }
-              },
-              yaxis: {
-                lines: {
-                    show: false
-                }
-              }, 
-            },
-            stroke: {
-              curve: "stepline",
-            },
-            title: {
-              text: "Pseudoternary",
-              align: "center",
-              style: {
-                fontSize:  "18px",
-                fontWeight:  600,
-                color:  "black",
-              },
-            },
-          }}
-          series={[
-            {
-              name: "series-1",
-              data: dataPseudo,
-            },
-          ]}
-          type="line"
-          width={1000}
-          height={200}
-        />
+              <Text
+                w="100%"
+                mb="8px"
+                m="2"
+                color="#4E3B9D"
+                alignSelf="flex-start"
+                fontFamily="Rubik"
+                fontWeight="500"
+              >
+                Conversão
+              </Text>
 
-        <Chart
-          options={{
-            colors: ["#ff8c00"],
-            chart: {
-              id: "4B3T-example",
-              toolbar: {
-                show: false,
-              },
-              fontFamily: "Rubik",
-            },
-            markers: {
-              size: 5,
-              colors: "orangered",
-            },
-            xaxis: {
-              type: "category",
-              categories: category4B3T,
-              labels: {
-                offsetX: 13,
-                offsetY: -5,
-              },
-              position: "top",
-            },
-            yaxis: {
-              tickAmount: 2,
-              labels: {
-                style: {
-                  colors: ["red", "black", "green"],
-                  fontSize: "16px",
+              <Input
+                w="100%"
+                borderColor="#56CFE1"
+                focusBorderColor="#56CFE1"
+                errorBorderColor="red"
+                placeholder="Código"
+                _placeholder={{
+                  opacity: 1,
+                  color: "gray.500",
                   fontFamily: "Rubik",
-                  fontWeight: 500,
-                  cssClass: "apexcharts-yaxis-label",
-                },
-                formatter: function (value) {
-                  if (value === 1) {
-                    value = "+";
-                    return value;
-                  } else if (value === -1) {
-                    value = "-";
-                    return value;
-                  } else {
-                    value = "0";
-                    return value;
-                  }
-                },
-              },
-            },
-            grid: {
-              xaxis: {
-                lines: {
-                    show: true
-                }
-              },
-              yaxis: {
-                lines: {
-                    show: false
-                }
-              }, 
-            },
-            stroke: {
-              curve: "stepline",
-            },
-            title: {
-              text: "4B3T",
-              align: "center",
-              style: {
-                fontSize:  "18px",
-                fontWeight:  600,
-                color:  "black",
-              },
-            },
-          }}
-          series={[
-            {
-              name: "series-1",
-              data: data4B3T,
-            },
-          ]}
-          type="line"
-          width={1000}
-          height={200}
-        />
-      </div>
-    </>
+                  fontWeight: 300,
+                }}
+                borderRadius="15"
+                color="#4E3B9D"
+                fontFamily="Rubik"
+                fontWeight="300"
+                size="md"
+                width="auto"
+              />
+            </Center>
+            
+
+            <VStack w="100%" mt="10" m="5" p="3">
+              <Button bg="#FB6D37" color="white" variant="solid" h="50px" w="100%" borderRadius="20">
+                NRZI
+              </Button>
+              <Button bg="#FB6D37" color="white" variant="solid" h="50px" w="100%" borderRadius="20">
+                Pseudoternary
+              </Button>
+              <Button bg="#FB6D37" color="white" variant="solid" h="50px" w="100%" borderRadius="20">
+                4B3T
+              </Button>
+            </VStack>
+          </Center>
+        </Center>
+        
+        <Flex h="100%" bg="#F8F8F7" borderTopRightRadius="18" borderBottomRightRadius="18">
+          <VStack
+            flex="1"
+            align="stretch"
+            justify="center"
+            direction="column"
+            m="2"
+            ml="8"
+            mr="8"
+          >
+            <Center bg="white" p="2" pt="5" borderRadius="10" shadow="xl">
+              <PulseChart
+                strokeColor="#00bfff"
+                idChart="Nrzi-example"
+                categoryX={categoryNrzi}
+                offsetXLabels={8}
+                tickAmountY={1}
+                colorsYLabels={["orangered", "limegreen"]}
+                titleChart="NRZI"
+                dataChart={dataNrzi}
+              />
+            </Center>
+
+            <Center bg="white" pt="3" borderRadius="10" shadow="xl">
+              <PulseChart
+                strokeColor="#ff69b4"
+                idChart="pseudo-example"
+                categoryX={categoryPseudo}
+                offsetXLabels={8}
+                tickAmountY={2}
+                colorsYLabels={["orangered", "#4E3B9D", "limegreen"]}
+                titleChart="Pseudoternary"
+                dataChart={dataPseudo}
+              />
+            </Center>
+
+            <Center bg="white" pt="3" borderRadius="10" shadow="xl">
+              <PulseChart
+                strokeColor="#ff8c00"
+                idChart="4B3T-example"
+                categoryX={category4B3T}
+                offsetXLabels={10}
+                tickAmountY={2}
+                colorsYLabels={["orangered", "#4E3B9D", "limegreen"]}
+                titleChart="4B3T"
+                dataChart={data4B3T}
+              />
+            </Center>
+          </VStack>
+        </Flex>
+        
+      </Center>
+    </Flex>
   );
 };
 export default App;
---
+++ src/components/PulseChart.js
@@ -0,0 +1,106 @@
+import Chart from "react-apexcharts";
+
+export default function PulseChart(props) {
+  return (
+    <Chart
+      options={{
+        colors: [props.strokeColor],
+        chart: {
+          id: props.idChart,
+          toolbar: {
+            show: false,
+          },
+          fontFamily: "Rubik",
+        },
+        markers: {
+          size: 0,
+        },
+        xaxis: {
+          type: "category",
+          categories: props.categoryX,
+          labels: {
+            style: {
+              colors: "#4E3B9D",
+              fontSize: "12px",
+              fontWeight: 400,
+              cssClass: "apexcharts-yaxis-label",
+            },
+            offsetX: props.offsetXLabels,
+            offsetY: -5,
+          },
+          position: "top",
+          axisBorder: {
+            show: false,
+          },
+          axisTicks: {
+            show: false,
+          },
+        },
+        yaxis: {
+          tickAmount: props.tickAmountY,
+          labels: {
+            style: {
+              colors: props.colorsYLabels,
+              fontSize: "14px",
+              fontWeight: 400,
+              cssClass: "apexcharts-yaxis-label",
+            },
+            offsetX: -5,
+            formatter: function (value) {
+              if (value === 1) {
+                value = "+";
+                return value;
+              } else if (value === -1) {
+                value = "-";
+                return value;
+              } else {
+                value = "0";
+                return value;
+              }
+            },
+          },
+        },
+        grid: {
+          strokeDashArray: 4.5,
+          borderColor: "#4E3B9D",
+          padding: {
+              top: 10,
+          },
+          xaxis: {
+            lines: {
+              show: true,
+            },
+          },
+          yaxis: {
+            lines: {
+              show: false,
+            },
+          },
+        },
+        stroke: {
+          curve: "stepline",
+          lineCap: "round",
+          width: 3,
+        },
+        title: {
+          text: props.titleChart,
+          align: "left",
+          style: {
+            fontSize: "18px",
+            fontWeight: 400,
+            color: "#4E3B9D",
+          },
+        },
+      }}
+      series={[
+        {
+          name: "series-1",
+          data: props.dataChart,
+        },
+      ]}
+      type="line"
+      width={800}
+      height={160}
+    />
+  );
+}
